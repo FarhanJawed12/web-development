024fea8a921ceec02bf93d1d025b697c5d6a302a
Farhanjawed12 <farhanjawed1222@gmail.com>
2025-02-21 17:23:43 +0530

    web dev

---
+++ Javascript Fundamentals /01-script.js
@@ -0,0 +1,3 @@
+console.log("Hello world");
+console.log("Learning JS");
+// JS is a dynamically typed language where the type checking is performed ot runtime
---
+++ Javascript Fundamentals /02-script.js
@@ -0,0 +1,21 @@
+// variable is JS
+fullName = "FJiron";
+age = 24;
+price = 99.99;
+a = null;
+b = undefined;
+isFollow = true;
+
+console.log(fullName);
+console.log(age);
+console.log(price);
+console.log(a);
+console.log(b);
+console.log(isFollow);
+
+//Variables- variables are container that store data
+//Variable rules:
+//1. Variable names are case sensitive
+//2. Only letters, digit, underscore and $ is allowed (not even space)
+//3. Only a letter, underscore or $ should be 1st character
+//4. Reserved words cannot be variable names
