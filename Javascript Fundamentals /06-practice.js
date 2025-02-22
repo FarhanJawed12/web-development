b89e7e221e592fb18bf8840fa99af5f4807b97a4
Farhanjawed12 <farhanjawed1222@gmail.com>
2025-02-22 13:29:13 +0530

    practice 2

---
+++ Javascript Fundamentals /06-practice.js
@@ -0,0 +1,11 @@
+// Create a const object called profile to store information 
+
+const profile = {
+    username: "ironman12",
+    totalPost: 195,
+    totalFollower: 569000,
+    totalFollow: 10,
+    isfollow: false,
+    Detail: "wear iron suit| member of marvel universe | died fighting Thanos",
+};
+console.log(profile);
