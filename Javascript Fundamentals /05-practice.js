48ddaf1fa61872ac34a6aa3100178745a8b337ff
Farhanjawed12 <farhanjawed1222@gmail.com>
2025-02-22 13:28:52 +0530

    practice 1

---
+++ Javascript Fundamentals /05-practice.js
@@ -0,0 +1,12 @@
+//Create a const like object called product to store info
+
+const product = {
+    title : "Parker Jotter Standard CT Ball Pen (Black)",
+    rating : 4,
+    isDeal : true,
+    price : 270,
+    offer : 5
+};
+console.log(product);
+
+
