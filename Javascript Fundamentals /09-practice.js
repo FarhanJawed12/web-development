5201db43f9cc2e700b419672991e09fd620375f2
Farhanjawed12 <farhanjawed1222@gmail.com>
2025-02-22 20:03:07 +0530

    conditionalstatements

---
+++ Javascript Fundamentals /09-practice.js
@@ -0,0 +1,12 @@
+// Get user to input a number using promt("Enter a number:"). check if the number is a multiple of 5 0r not
+
+let num = 25;
+
+if(num % 5 === 0) {
+    console.log(num,"is a multiple of 5")
+}
+
+else {
+    console.log(num,"is not a multiple of 5")
+}
+
